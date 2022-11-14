330e5e40cbea21a13173a39a504630e90a909ea1
Manny <manuelpo.msn.com>
2022-11-14 16:48:56 -0700

    update with if statement

---
+++ intro.js
@@ -0,0 +1,18 @@
+console.log("Hello Word");
+const a = 5;
+console.log(a);
+
+let b = 3;
+console.log(b);
+
+let c = 9;
+console.log(c);
+
+// if statement
+let x = 5;
+
+if (x > 3) {
+  console.log("greater than 3");
+} else {
+  console.log("3 or less");
+}
